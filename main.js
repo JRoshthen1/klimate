802421d7b1db0e212cf49cd3dcd15e2d1c9ff6f5
JRoshthen1 <jroshthen1@gmail.com>
2022-05-29 20:54:13 +0200

    init

---
+++ main.js
@@ -0,0 +1,63 @@
+
+function pos0() {
+    document.getElementById("q0").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+
+function pos1() {
+    document.getElementById("q1").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+
+function pos2() {
+    document.getElementById("q2").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos3() {
+    document.getElementById("q3").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos4() {
+    document.getElementById("q4").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos5() {
+    document.getElementById("q5").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos6() {
+    document.getElementById("q6").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos7() {
+    document.getElementById("q7").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos8() {
+    document.getElementById("q8").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos9() {
+    document.getElementById("q9").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos10() {
+    document.getElementById("q10").scrollIntoView({
+        behavior: "smooth",
+      });
+}
+function pos11() {
+    document.getElementById("q11").scrollIntoView({
+        behavior: "smooth",
+      });
+}
